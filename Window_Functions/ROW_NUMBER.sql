2e10d0b6f75018370e54725aee708ab1b4b48366
Emir Aygül <78544579+DionyTaken@users.noreply.github.com>
2025-05-10 01:33:39 +0300

    Update ROW_NUMBER.sql

--- Window_Functions/ROW_NUMBER.sql
+++ Window_Functions/ROW_NUMBER.sql
@@ -1,4 +1,4 @@
-SELECT customer_name, sale_amount,
-       ROW_NUMBER() OVER (ORDER BY sale_amount DESC) AS row_num
+SELECT Customers.customer_name, Sales.sale_amount,
+       ROW_NUMBER() OVER (ORDER BY Sales.sale_amount DESC) AS row_num
 FROM Sales
 JOIN Customers ON Sales.customer_id = Customers.customer_id;
